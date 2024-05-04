4622e88fd68d7b05025a0b28759cd255a5671fd1
Mohammad-Khobaib <150820696+Mohammad-Khobaib@users.noreply.github.com>
2024-05-04 21:01:20 +0600

    Unit tests for forward_list class

--- forward_list/forward_list_test.cpp
+++ forward_list/forward_list_test.cpp
@@ -4,7 +4,7 @@
 #include "forward_list.hpp"
 
 void test_constructor_and_push_front() {
-    forward_list<int> myList;
+    my_stl::forward_list<int> myList;
     assert(myList.empty());
 
     myList.push_front(3);
@@ -19,7 +19,7 @@ void test_constructor_and_push_front() {
 }
 
 void test_pop_front() {
-    forward_list<int> myList {1, 2, 3};
+    my_stl::forward_list<int> myList {1, 2, 3};
     myList.pop_front();
     assert(myList.front() == 2);
 
@@ -31,42 +31,42 @@ void test_pop_front() {
 }
 
 void test_remove() {
-    forward_list<int> myList {1, 2, 3, 2, 4, 2, 5};
+    my_stl::forward_list<int> myList {1, 2, 3, 2, 4, 2, 5};
     myList.remove(2);
     assert(myList.front() == 1);
     // After removing all occurrences of 2, the list should contain 3 elements
 }
 
 void test_clear() {
-    forward_list<int> myList {1, 2, 3};
+    my_stl::forward_list<int> myList {1, 2, 3};
     myList.clear();
     assert(myList.empty());
 }
 
 void test_assign_with_iterators() {
-    forward_list<int> myList1 {1, 2, 3};
-    forward_list<int> myList2 {4, 5};
+    my_stl::forward_list<int> myList1 {1, 2, 3};
+    my_stl::forward_list<int> myList2 {4, 5};
     myList2.assign(myList1.begin(), myList1.end());
     assert(myList2.front() == 1);
     // After assigning, the size of myList2 should be the same as myList1
 }
 
 void test_assign_with_number_of_elements() {
-    forward_list<int> myList;
+    my_stl::forward_list<int> myList;
     myList.assign(3, 5);
     assert(myList.front() == 5);
     // After assigning, the list should contain 3 elements, all with value 5
 }
 
 void test_assign_with_initializer_list() {
-    forward_list<int> myList;
+    my_stl::forward_list<int> myList;
     myList.assign({1, 2, 3});
     assert(myList.front() == 1);
     // After assigning, the list should contain elements 1, 2, and 3
 }
 
 void test_iterator() {
-    forward_list<int> myList {1, 2, 3, 4, 5};
+    my_stl::forward_list<int> myList {1, 2, 3, 4, 5};
     int sum = 0;
     for (auto it = myList.begin(); it != myList.end(); ++it) {
         sum += *it;
