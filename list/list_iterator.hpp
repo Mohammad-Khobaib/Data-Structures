cd4daf7ecb68512e0f638b651e5e00336d8ef754
Mohammad-Khobaib <150820696+Mohammad-Khobaib@users.noreply.github.com>
2024-05-06 19:56:32 +0600

    list_iterator class template implementation

--- list/list_iterator.hpp
+++ list/list_iterator.hpp
@@ -1,27 +1,54 @@
 #pragma once
 
-#include "node.hpp"
+#include "../utility/is_const.hpp"
+#include "../utility/reference_selector.hpp"
 
-template<typename list>
+namespace my_stl
+{
+template<typename List>
 class list_iterator {
 public:
-    using T = typename list::value_type;
-private:
-    node<T> *element;
-public:
-    list_iterator(node<T>* element) : element(element) {}
-    list_iterator& operator++() {
-        element = element -> next;
+    using value_type = typename List::value_type;
+    using node = typename List::node;
+    using pointer = typename List::pointer;
+    using reference = typename reference_selector<List, is_const<List>::value>::reference;
+    using difference_type = typename List::difference_type;
+    using const_reference = typename List::const_reference;
+
+    node* m_node;
+
+    list_iterator<List>(node* m_node) : m_node(m_node) {}
+
+    list_iterator<List>(const list_iterator<List>& right) : m_node(right.m_node) {}
+
+    list_iterator<List>& operator++() {
+        m_node = m_node -> next;
         return *this;
     }
-    bool operator!=(list_iterator& other) {
-        return element != other.element;
-    }
-    T& operator*() {
-        return element -> data;
+
+    list_iterator<List> operator++(int) {
+        list_iterator<List> copy = *this;
+        ++(*this);
+        return copy;
     }
 
-    node<T> *getNode() {
-        return element;
+    list_iterator<List>& operator--() {
+        m_node = m_node -> prev;
+        return *this;
+    }
+
+    list_iterator<List> operator--(int) {
+        list_iterator<List> copy = *this;
+        --(*this);
+        return copy;
+    }
+
+    bool operator!=(const list_iterator<List>& right) const {
+        return m_node != right.m_node;
+    }
+
+    reference operator*() const {
+        return m_node -> data;
     }
 };
+}
