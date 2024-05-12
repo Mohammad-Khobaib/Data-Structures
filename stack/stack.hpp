0cd7bffba9a4f1a54cbe79015150a4ebc8675ec8
Mohammad-Khobaib <150820696+Mohammad-Khobaib@users.noreply.github.com>
2024-05-12 19:32:19 +0600

    Update stack.hpp

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../forward_list/forward_list.hpp"
+#include "../utility/move.hpp"
 
 namespace my_stl
 {
@@ -15,13 +16,33 @@ private:
     forward_list<T> m_forward_list;
     size_type m_size;
 public:
-    stack() : m_forward_list(), m_size(0) {}
+    // Default constructor
+    stack<value_type>() : 
+        m_forward_list(),
+        m_size(0)
+    {}
     
+    // Copy constructor
+    stack<value_type>(const stack<value_type>& right) :
+        m_forward_list(right.m_forward_list),
+        m_size(right.m_size)
+    {}
+
+    // Move constructor
+    stack<value_type>(stack<value_type>&& right) :
+        m_forward_list(move(right.m_forward_list)),
+        m_size(right.m_size)
+    {
+        right.m_size = 0;
+    }
+
+    // Inserts a new element at the top of the stack.
     void push(const_reference val) {
         m_forward_list.push_front(val);
         m_size++;
     }
 
+    // Removes the top element from the stack.
     void pop() {
         if (!m_forward_list.empty()) {
             m_forward_list.pop_front();
@@ -29,16 +50,34 @@ public:
         }
     }
 
-    const_reference top() {
+    // Returns a reference to the top element of the stack
+    reference top() {
         return m_forward_list.front();
     }
 
+    // Returns a const reference to the top element of the stack
+    const_reference top() const {
+        return m_forward_list.front();
+    }
+
+    // Returns the number of elements in the stack.
     size_type size() const {
         return m_size;
     }
 
+    // Returns the number of elements in the stack.
     bool empty() const {
         return m_forward_list.empty();
     }
+
+    // Compares if the current stack is not equal to the given stack.
+    bool operator!=(const stack<value_type>& right) const {
+        return m_forward_list != right.m_forward_list;
+    }
+
+    // Compares if the current stack is equal to the given stack.
+    bool operator==(const stack<value_type>& right) const {
+        return m_forward_list == right.m_forward_list;
+    }
 };
-}
+} // namespace my_stl
